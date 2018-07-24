f0d165469b5a8f1ac13a7c5c6269fdfda7942368
Carrie Savinell <carrielane89@gmail.com>
2018-07-24 18:34:06 +0000

    changes to signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,48 @@
 class UsersController < ApplicationController
 
   # GET: /users
-  get "/users/:slug" do
-    @user = User.find_by_slug(params[:slug])
-    erb :"/users/show"
-  end
+  # get "/users/:slug" do
+  #   @user = User.find_by_slug(params[:slug])
+  #   erb :"/users/show"
+  # end
 
   #GET: signup
-  # GET: login
   get '/signup' do
-    if !is_logged_in?
-      erb :'/users/signup'
+    if is_logged_in?
+      redirect :'/users/#{@user.slug}'
     else
-      redirect :'/trails'
+      erb :'/users/signup'
     end
   end
 
   #POST: signup
   post '/signup' do
     if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect :"/signup"
+      redirect :signup
     else
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
       redirect :'/trails'
     end
-  end            
+  end
+
+  get '/login' do
+    if !is_logged_in?
+      erb :login
+    else
+      redirect :'/trails'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect :'/trails'
+    else
+      redirect :'/signup'
+    end
+  end
 
   # GET: /users/new
   get "/users/new" do
@@ -55,5 +72,10 @@ class UsersController < ApplicationController
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
     redirect "/users"
+  end
+
+  get '/logout' do
+    session.clear
+    redirect :login
   end
 end
